bba44e4cd7df678e15244853f20c7faa331bb892
simeo russo <simeo94@hotmail.com>
2020-11-23 15:48:00 +0000

    started to create json server and implement on create stream comp

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -4,21 +4,46 @@ import {Field, reduxForm} from 'redux-form';
 class StreamCreate extends React.Component {
 
   renderInput(formProps){
-    return <input type="text" {...formProps.input} />
+    return(
+      <div className="field">
+        <label>{formProps.label}</label>
+        <input type="text" {...formProps.input} autoComplete="off"/>
+        <div>{formProps.meta.error}</div>
+      </div>
+    );
+  }
+
+  onSubmit(formValues){
+   console.log(formValues)
   }
 
   render(){
     return(
-      <div>
-        <form>
-          <Field name="title" component={this.renderInput}/>
-          <Field name="description" component={this.renderInput}/>
+      <div className="ui container">
+        <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <Field name="title" label="Enter Title" component={this.renderInput}/>
+          <Field name="description" label="Enter Description" component={this.renderInput}/>
+          <button className="ui button primary">Submit</button>
         </form>
       </div>
     );
   }
 };
 
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title){
+    errors.title = "you must enter a title"
+  }
+  if (!formValues.description){
+    errors.description = "you must enter a description"
+  }
+
+  return errors;
+}
+
 export default reduxForm({
-  form: 'StreamCreate'
+  form: 'StreamCreate',
+  validate
 }) (StreamCreate);
