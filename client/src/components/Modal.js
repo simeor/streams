75518cde9a33edbaa37b275448f06e2bbce78e9d
simeo russo <simeo94@hotmail.com>
2020-12-02 17:56:05 +0000

    delete model created, need to implement action on delete button

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import history from '../history';
 
 const Modal = (props) => {
   return ReactDom.createPortal(
-    <div className="ui dimmer modals visable active">
-      <div className="ui standard modal visable active">
-        lorem ipsum lorem ipsum lorem ipsum
+    <div onClick={ () => history.push('/')} className="ui dimmer modals visable active">
+      <div onClick={(e) => e.stopPropagation()} className="ui standard modal visable active">
+        <div className="header">{props.title}</div>
+        <div className="content">
+          {props.content}
+        </div>
+        <div className="actions">
+          {props.actions}
+        </div>
       </div>
     </div>
     ,document.querySelector('#modal'));
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {Fragment} from "react";
 import Modal from '../Modal';
 
+
 const StreamDelete = () => {
+
+  const actions = (
+    <Fragment>
+       <button className="ui button red">Delete</button>
+        <button className="ui button">Cancel</button>
+    </Fragment>
+   );
+
   return(
     <div>
       StreamDelete comp
-      <Modal />
+      <Modal
+      title="Delete Stream"
+      content = "Are you sure you want to delete this streat"
+      actions={actions}
+      />
     </div>
   );
 };
